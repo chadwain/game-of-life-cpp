2ad449f2b5cb0f2383a5941895160156f4c6cdad
Chadwain <chadwainholness@gmail.com>
2020-07-12 15:45:02 -0400

    Initial commit

---
+++ README.txt
@@ -0,0 +1,2 @@
+# Game of Life
+Originally created by John Conway. Look [here](https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life) for reference.
---
+++ default.nix
@@ -0,0 +1,6 @@
+with import <nixpkgs> {};
+stdenv.mkDerivation {
+  name = "game-of-life";
+  nativeBuildInputs = [ cmake ];
+  buildInputs = [];
+}
